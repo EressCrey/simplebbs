b6ed47cea3d76ec72e6100d60c82795c722dd52f
lonelykkk <140876474+lonelykkk@users.noreply.github.com>
2024-12-17 20:40:59 +0800

    前端代码

--- kkbbs-front/kkbbs-front-admin/vite.config.js
+++ kkbbs-front/kkbbs-front-admin/vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     port: 3005,
     proxy: {
       "/api": {
-        target: "http://localhost:7071/",
+        target: "http://127.0.0.1:7071/",
         changeOrigin: true,
         pathRewrite: {
           "^api": "/api"
