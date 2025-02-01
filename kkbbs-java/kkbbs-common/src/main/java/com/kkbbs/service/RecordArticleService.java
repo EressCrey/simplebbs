6069e0974f861084f9903ed198df7cf26fd7e27d
lonelykkk <2765314967@qq.com>
2025-02-02 00:58:49 +0800

    修复无法运行的问题

--- kkbbs-java/kkbbs-common/src/main/java/com/kkbbs/service/RecordArticleService.java
+++
@@ -1,5 +0,0 @@
-package com.kkbbs.service;
-
-public interface RecordArticleService {
-
-}
--- kkbbs-java/kkbbs.sql
+++ kkbbs-java/kkbbs.sql
@@ -5,7 +5,7 @@
  Source Server Type    : MySQL
  Source Server Version : 50719 (5.7.19)
  Source Host           : localhost:3306
- Source Schema         : kkbbs
+ Source Schema         : easybbs
 
  Target Server Type    : MySQL
  Target Server Version : 50719 (5.7.19)
